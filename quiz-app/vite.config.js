32b251b5fa54738b2d1e5e85ce3f5a641ba8fc32
Mo kaif <kaifmd0808@gmail.com>
2026-02-02 19:40:58 +0530

    Change base path in Vite config to lowercase

--- quiz-app/vite.config.js
+++ quiz-app/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Quiz-App/',
+  base: '/quiz-app/',
   plugins: [react()],
 })
